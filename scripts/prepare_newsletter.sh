a286d8c9d2877015aaaf566aa70ddc7440897ece
Thierry Berger <contact@thierryberger.com>
2023-11-17 21:21:56 +0100

    more debuf

--- scripts/prepare_newsletter.sh
+++ scripts/prepare_newsletter.sh
@@ -10,9 +10,12 @@ read -r -d '\n' -a news <<< `find content/news/ -type d -printf "%f\n"| sort -n`
 
 last_news=`echo ${news[-1]} | sed -r s/0//`
 echo $last_news
+echo $((last_news+1))
 news_to_create_simple=$((last_news+1))
+echo $news_to_create_simple
+printf "%#03s" $news_to_create_simple
 news_to_create=`printf "%#03s" $news_to_create_simple`
-
+echo $news_to_create
 read -e -p "Do you want to create issue $news_to_create? (y/n): " choice
 
 [[ "$choice" != [Yy]* ]] && exit
