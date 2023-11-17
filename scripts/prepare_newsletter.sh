aed690b97cd01770b3c0d315b015037e0a7c12b4
Thierry Berger <contact@thierryberger.com>
2023-11-17 21:48:27 +0100

    simpler script + fix branch deletion ?

--- scripts/prepare_newsletter.sh
+++ scripts/prepare_newsletter.sh
@@ -7,26 +7,22 @@
 set -v
 
 read -r -d '\n' -a news <<< `find content/news/ -type d -printf "%f\n"| sort -n`
-echo $news
-echo ${news[-1]}
 
-export last_news=`echo ${news[-1]} | sed -r s/0//`
-export news_to_create_simple=`echo $((last_news+1))`
-
-echo $news_to_create_simple
-printf "%#03s" $news_to_create_simple
+last_news=`echo ${news[-1]} | sed -r s/0//`
+news_to_create_simple=`echo $((last_news+1))`
 
 printf %03d $news_to_create_simple
 
-export news_to_create=$(printf %03d $news_to_create_simple)
+news_to_create=$(printf %03d $news_to_create_simple)
+
 echo $news_to_create
 read -e -p "Do you want to create issue $news_to_create? (y/n): " choice
 
 [[ "$choice" != [Yy]* ]] && exit
 
-echo "accepted"
+echo "accepted $news_to_create"
 
-export new_file=`echo "content/news/$news_to_create/index.md"`
+new_file=`echo "content/news/$news_to_create/index.md"`
 
 echo $new_file
 
